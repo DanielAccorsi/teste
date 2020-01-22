4c09ddbad9f8b8427258bc0dbe624d231dcb9c2d
ACCORSI <daniel.accorsi@atos.net>
2020-01-22 14:56:09 -0300

    banco

--- index.php
+++ index.php
@@ -14,4 +14,8 @@ phpinfo();
 $dbconn = pg_connect("host=".$servername." dbname=".getenv('DATABASE_NAME')." user=".$username." password=".$password)
     or die('Could not connect: ' . pg_last_error());
 
+$query = 'SELECT * FROM public.user';
+$result = pg_query($query) or die('Query failed: ' . pg_last_error());
+
+print_r($result);
 ?>
